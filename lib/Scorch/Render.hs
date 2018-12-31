8c9ba97989743f75fe12e332e7e7ac1460ff8ae5
Solonarv <nstamm@gmx.de>
2018-12-31 21:34:43 +0100

    more tinkering with OpenGL

--- lib/Scorch/Render.hs
+++ lib/Scorch/Render.hs
@@ -5,9 +5,9 @@ import Data.Foldable
 import Data.Int
 
 import Apecs hiding (($=))
-import Graphics.GL
 import Linear.V2
 import Linear.V4
+import SDL
 import UnliftIO
 
 import Apecs.Folds
@@ -15,33 +15,13 @@ import Scorch.Components
 
 -- TODO use shaders
 
-render :: (MonadIO m, Has w m Extent, Has w m Colored) => SystemT w m ()
-render = do
-  prims <- cfoldMap \(Extent pos dims, Colored (V4 r g b a)) -> do
-    glColor4f r g b a
-    rectTris pos dims
-  liftIO do
-    glClearColor 0.8 0.8 1 0
-    glClear GL_COLOR_BUFFER_BIT
-    glBegin GL_TRIANGLES
-    prims
-    glEnd
+render :: (MonadIO m, Has w m Extent, Has w m Colored) => SDL.Renderer -> SystemT w m ()
+render rdr = do
+  rendererDrawColor $= V4 127 255 255 255
+  clear rdr
+  cmapM_ \(Extent pos dims, Colored color) -> do
+    rendererDrawColor $= color
+    fillRect rdr (Just (rect pos dims))
 
--- | Build a rectangle by specifying two triangles.
--- Meant to be used between 'glBegin' and 'glEnd'.
-rectTris :: V2 Int32 -> V2 Int32 -> IO ()
-rectTris (fmap fromIntegral -> V2 x y) (fmap fromIntegral -> V2 dx dy) = do
-    vertex2 ul
-    vertex2 bl
-    vertex2 ur
-    vertex2 ur
-    vertex2 bl
-    vertex2 br
-  where
-    bl = V2 x      y
-    br = V2 (x+dx) y
-    ul = V2 x      (y+dy)
-    ur = V2 (x+dx) (y+dy)
-
-vertex2 :: V2 Float -> IO ()
-vertex2 (V2 x y) = glVertex2f x y
+rect :: V2 a -> V2 a -> SDL.Rectangle a
+rect pos dims = Rectangle (P pos) dims
--- src/Main.hs
+++ src/Main.hs
@@ -24,31 +24,27 @@ main :: IO ()
 main = do
   SDL.initializeAll
   window <- SDL.createWindow "Scorch Test" SDL.defaultWindow
-    { SDL.windowOpenGL = Just SDL.defaultOpenGL
-        { SDL.glColorPrecision = V4 8 8 8 8
-        , SDL.glProfile        = SDL.Core SDL.Normal 3 2
-        }
-    , SDL.windowInitialSize = V2 640 480
+    { SDL.windowInitialSize = V2 640 480
     }
-  ctx <- SDL.glCreateContext window
-  SDL.glMakeCurrent window ctx
-  prog <- initResources
+  rdr <- SDL.createRenderer defaultRenderer
+    { SDL.rendererType = SDL.AcceleratedVSyncRenderer
+    , SDL.rendererTargetTexture = True
+    }
   world <- initTestWorld
   runWith world setupTestScene
   let
     loop = do
       events <- fmap SDL.eventPayload <$> SDL.pollEvents
       whenEmpty [ () | SDL.WindowClosedEvent _ <- events ] do
-        runWith world Scorch.render
-        SDL.glSwapWindow window
+        runWith world (Scorch.render rdr)
         threadDelay 20_000
         loop
   loop
 
 setupTestScene :: SystemT TestWorld IO ()
 setupTestScene = do
-  newEntity (Extent (V2 200 300) (V2 100 60), Colored (V4 1 0 0 0))
-  newEntity (Extent (V2 170 320) (V2 80 200), Colored (V4 0 1 0 0))
+  newEntity (Extent (V2 200 300) (V2 100 60), Colored (V4 255 0 0 255))
+  newEntity (Extent (V2 170 320) (V2 80 200), Colored (V4 0 255 0 255))
   pure ()
 
 data TestWorld = TestWorld
@@ -71,27 +67,3 @@ initTestWorld = TestWorld <$> explInit <*> explInit <*> explInit
 
 whenEmpty :: (Foldable t, Applicative f) => t a -> f () -> f ()
 whenEmpty = when . null
-
-initResources :: IO Program
-initResources = do
-  -- VAO
-  vao <- overPtr $ glGenVertexArrays 1
-  glBindVertexArray vao
-  -- VBO
-  vbo <- overPtr $ glGenBuffers 1
-  glBindBuffer GL_ARRAY_BUFFER vbo
-  
-  -- shader program
-  Program program <- attachShaders =<< sequenceA [shVertex2D, shFragment2D]
-  glUseProgram program
-
-  -- Link Vertex data with Attributes
-  posAttrib <- withCString "position" $ glGetAttribLocation program
-  glVertexAttribPointer (fromIntegral posAttrib) 2 GL_FLOAT GL_FALSE 0 nullPtr
-  glEnableVertexAttribArray (fromIntegral posAttrib)
-
-  -- Uniforms
-  uniColor <- withCString "triangleColor" $ glGetUniformLocation program
-  glUniform3f uniColor 0 0 1
-
-  pure (Program program)
